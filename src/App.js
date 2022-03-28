1882bbde86d4cc8fb93ad4cc85e3893e06589ab1
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-03-28 21:31:14 +0600

    add new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header/Header";
+import Users from "./components/Users/Users"
+import Cards from "./components/Card/Card"
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Users/>
+     <Cards/>
     </div>
   );
 }
